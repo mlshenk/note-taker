fb0284b60d9cd1b5d1f4b9b9b7dee1d8a0a2ed37
Mike Shenk <mlshenk415@gmail.com>
2020-07-13 22:41:55 -0700

    fixed PORT to be process.env for Heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3040;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -12,13 +12,13 @@ app.use(express.json());
 
 app.get("/notes", function(req, res) {
     // res.send("Welcome to the Star Wars Page!")
-    res.sendfile(path.join(__dirname, "Develop/public/notes.html"));
+    res.sendfile(path.join(__dirname, "public/notes.html"));
   });
 
   app.get("*", function(req, res) {
     // res.send("Welcome to the home page!")
     // res.sendFile("./note-taker/Develop/public/index.html");
-    res.sendfile(path.join(__dirname, "Develop/public/index.html"));
+    res.sendfile(path.join(__dirname, "public/index.html"));
   });
 
 
