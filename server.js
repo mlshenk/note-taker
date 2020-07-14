1fcc3619c147320030954feda6c98f591a20643e
Mike Shenk <mlshenk415@gmail.com>
2020-07-13 19:35:12 -0700

    finished server.js

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3040;
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.get("/api/notes", async (req, res) => {
+    res.json(await DB.readNotes())
+});
 
-app.get("/notes", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.sendfile(path.join(__dirname, "Develop/public/notes.html"));
-  });
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-  app.get("*", function(req, res) {
-    // res.send("Welcome to the home page!")
-    // res.sendFile("./note-taker/Develop/public/index.html");
-    res.sendfile(path.join(__dirname, "Develop/public/index.html"));
-    
+app.post("/api/notes", async function (req, res) {
+    const newNote = req.body
+    const currentNotes = await DB.readNotes();
+    await DB.writeNotes(newNote, currentNotes)
+    return res.json("Success");
+});
 
-  });
+app.delete('/api/notes/:id', async (req,res) => {
+    const requestedID = req.params.id;
+    const currentNotes = await DB.readNotes();
+    await DB.deleteJSON(currentNotes, requestedID)
+    res.json("Success");
+})
 
-  app.listen(PORT, function() {
-    console.log("App listening on PORT http://localhost:" + PORT);
-  });
+app.listen(PORT, function () {
+    console.log("listening on Port: " + PORT)
+})
